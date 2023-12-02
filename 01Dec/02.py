a346aa856fa7f47b86147076b624561826b7f8dd
husainvj <husainvj@gmail.com>
2023-12-02 20:44:38 +0100

    start of day2

--- 01Dec/02.py
+++ 01Dec/02.py
@@ -5,10 +5,15 @@ import re
 
 
 # Using readlines()
-file = open('01Dec/input.txt', 'r')
+file = open('01Dec/temp.txt', 'r')
 Lines = file.readlines()
 
-num_dict={"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6","seven":"7","eight":"8","nine":"9","ten":"10"}
+num_dict={"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6","seven":"7","eight":"8","nine":"9"}
+
+number_dict = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6","seven":"7","eight":"8","nine":"9","ten":"10", 
+        "eleven":"11", "twelve":"12", "thirteen":"13", "fourteen":"14", "fifteen":"15", "sixteen":"16", "seventeen":"17", "eighteen":"18", "nineteen":"19", 
+        "twenty":"20", "thirty":"30", "forty":"40", "fifty":"50", "sixty":"60", "seventy":"70", "eighty":"80", "ninety":"90", 
+        "hundred":"00", "thousand":"000", "million":"000000", "billion":"000000000", "trillion":"000000000000"}
 
 for i in range(len(Lines)):
     matched={}
@@ -19,16 +24,23 @@ for i in range(len(Lines)):
             matched.update({x.start():x.group()})
 
     z=dict(sorted(matched.items()))
-    z
+    
+    if z:
+        a=list(z.values())[0]
+        a_=list(z.keys())[0]
+        b=list(z.values())[-1]
+        b_=list(z.keys())[-1]
 
+        #Lines[i]=Lines[i].insert(Lines[i][:a_], num_dict[a])
+        #Lines[i]=Lines[i].insert(Lines[i][b_:], num_dict[b])
+        Lines[i]=Lines[i][:a_]+num_dict[a]+Lines[i][a_:]
+        Lines[i]=Lines[i][:b_]+num_dict[b]+Lines[i][b_:]
+    """
     for v in z.values():
         Lines[i] = Lines[i].replace(v, num_dict[v])
-
-#print(Lines)
-
-
-
-
+    """
+print(Lines)
+len(Lines)
 
 integers = []
 count = 0
@@ -39,7 +51,9 @@ for line in Lines:
 
     integers.append(int(str))
     count += 1
-#print(integers)
+
+print(integers)
+print("total integers:", len(integers))
 print("sum of integers is : ", sum(integers))
 
 
